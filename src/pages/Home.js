7b8667ec81e3e7a13eaed7b2ac30c7045fb6bf28
sa2z <57865727+sa2z@users.noreply.github.com>
2021-10-15 00:38:10 +0900

    table component in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React from "react";
+import DataTable from "react-data-table-component";
 
+const columns = [
+  {
+    name: "No",
+    selector: (row) => row.id,
+    sortable: true
+  },
+  {
+    name: "Category",
+    selector: (row) => row.category,
+    sortable: true
+  },
+  {
+    name: "Kind",
+    selector: (row) => row.kind,
+    sortable: true
+  },
+  {
+    name: "Model",
+    selector: (row) => row.model,
+    sortable: true
+  },
+  {
+    name: "Version",
+    selector: (row) => row.version,
+    sortable: true
+  },
+  {
+    name: "Describe",
+    selector: (row) => row.describe,
+    sortable: true
+  }
+];
+
+const data = [
+  {
+    id: 1,
+    category: "Vision",
+    kind: "Classification",
+    model: "Classify_Person.pt",
+    version: "1.0.0",
+    describe: "classificate people"
+  },
+  {
+    id: 2,
+    category: "Vision",
+    kind: "Detection",
+    model: "Detect_Person.pt",
+    version: "1.0.1",
+    describe: "Bounding box detect people"
+  }
+];
+
+// function MyComponent() {
+//     return (
+//         <DataTable
+//             columns={columns}
+//             data={data}
+//         />
+//     );
+// };
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
-    </div>
+    <DataTable columns={columns} data={data} />
+    // <div className='home'>
+    //   <h1>Home</h1>
+    // </div>
   );
 }
 
